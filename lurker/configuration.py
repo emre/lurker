0597d7e072ce1b56ca3d3f1fca67efa493337d21
Emre Yılmaz <mail@emreyilmaz.me>
2012-01-09 21:03:03 +0200

    added base configuration object

---
+++ lurker/configuration.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+class BaseConfig(object):
+    """
+    Base Configuration object for MySQL connections.This class is not intended to be used in this form. Another
+    configuration object that extends this/BaseConfig must be passed to Connection object.
+    """
+
+    # main configuration info
+    host = 'localhost'
+    user = 'root'
+    passwd = ''
+    db = ''
+    port = 3306
+
+    # mysql-python options
+    use_unicode = True
+    charset = 'utf8'
+
+    configuration_object_check = True
+
+
+
